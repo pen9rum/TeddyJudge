e6198c3a38ee7a0b7a61bfbb94168090848d991f
Jack Lee <jack20499@gmail.com>
2023-04-25 23:11:33 +0800

    add an example for testing teddyjudge

--- backend/src/main/java/com/Teddy/backend/model/JavaCodeExecutor.java
+++ backend/src/main/java/com/Teddy/backend/model/JavaCodeExecutor.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JavaCodeExecutor {
@@ -22,15 +25,37 @@ public class JavaCodeExecutor {
                 // 編譯出錯，返回編譯錯誤信息
                 return "編譯錯誤：" + new String(compileProcess.getErrorStream().readAllBytes());
             }
-
+            
             // 運行已編譯的 Java 程序
             ProcessBuilder runProcessBuilder = new ProcessBuilder("java", "TempSolution");
             Process runProcess = runProcessBuilder.start();
+
+            // 將需要輸入的數據寫入標準輸入流
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(runProcess.getOutputStream(), StandardCharsets.UTF_8))) {
+                writer.write("10 40\n");
+                writer.flush();
+            }
+
             int runExitCode = runProcess.waitFor();
+            String ans = "Please input two numbers(use space to split):1-A\n" +
+                    "Gap: 30\n" +
+                    "Mul: 400\n" +
+                    "Avg: 25.0\n" +
+                    "Min: 10\n";
 
             if (runExitCode == 0) {
                 // 成功運行，返回輸出結果
-                return "輸出：" + new String(runProcess.getInputStream().readAllBytes());
+                String output = new String(runProcess.getInputStream().readAllBytes());
+                if(output.equals(ans)){
+                    System.out.println("Answer right!");
+                    return "AC";
+                }else{
+                    System.out.println("Answer wrong");
+                    System.out.println("Your answer is: "+output);
+                    System.out.println("But the right answer is: "+ ans);
+                    return "WA";
+                }
+
             } else {
                 // 運行時錯誤，返回錯誤信息
                 return "運行時錯誤：" + new String(runProcess.getErrorStream().readAllBytes());
